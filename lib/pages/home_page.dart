7fb1e0d7819ce1821d195cc1d2dcf766f7fc8173
ilham <ilhamafandi21@gmail.com>
2025-10-08 00:07:46 +0700

    10/08/2025

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -29,7 +29,7 @@ class HomePage extends StatelessWidget  {
             ),
             child: const Center(
               child: Text(
-                "12:45\",
+                "12:45\ntersisa",
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
               ),
