8ddab83c685f27a2d2999551a938255cfb2534e4
ilham <ilhamafandi21@gmail.com>
2025-10-08 01:12:17 +0700

    10-08-2025

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'pages/history_page.dart';
-import 'pages/home_page.dart';
-import 'pages/settings_page.dart';
+import 'pages/main_page.dart';
+
+
+
+
 
 void main() {
   runApp(const MyApp());
@@ -9,54 +11,13 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      title: 'Flutter',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      title: "First App",
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: const MainPage(),
     );
   }
 }
-
-class MainPage extends StatefulWidget {
-  const MainPage({super.key});
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage>{
-  int _selectedIndex = 0;
-
-  final List<Widget> _pages = const [
-      HomePage(),
-      HistoryPage(),
-      SettingsPage(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      body: _pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.history), label: "History"),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings"),
-        ],
-      ),
-    );
-  } 
-}
-
---
+++ lib/pages/main_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+import 'history_page.dart';
+import 'settings_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+
+  int _selectedIndex = 0;
+  
+  final List<Widget> _pages = [
+    // Center(child: Text("Halaman Home", style: TextStyle(fontSize: 24))),
+    // Center(child: Text("Halaman About", style: TextStyle(fontSize: 24))),
+
+    HomePage(),
+    HistoryPage(),
+    SettingsPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState((){
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: const Text("First App", style: TextStyle(color: Colors.blue))),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.info), label: "About"),
+          BottomNavigationBarItem(icon: Icon(Icons.info), label: "settings"),
+        ],
+      ),
+    );
+  }
+}
