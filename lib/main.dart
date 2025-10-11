fbce60acb2b9b18b1404dd8e20e58c34760a6f5d
ilham <ilhamafandi21@gmail.com>
2025-10-11 13:21:24 +0700

    11 oktober 2025

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      title: "First App",
+      title: "Aplikasi Pertama",
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const MainPage(),
     );
