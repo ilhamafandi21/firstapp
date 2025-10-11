2a5498035da520ded0e22ed80bf78744b68d7760
ilham <ilhamafandi21@gmail.com>
2025-10-11 13:22:00 +0700

    11 oktober 2025

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -7,7 +7,7 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(""),
+        title: const Text("Pengaturan"),
         centerTitle: true,
       ),
       body: Padding(
