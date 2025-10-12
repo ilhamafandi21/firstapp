de7595bce05b19d38fe873d75f5944458a85445b
ilham <ilhamafandi21@gmail.com>
2025-10-12 07:04:39 +0700

    12 oktober 2025

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -38,7 +38,7 @@ class _MainPageState extends State<MainPage> {
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
           BottomNavigationBarItem(icon: Icon(Icons.info), label: "About"),
           BottomNavigationBarItem(icon: Icon(Icons.info), label: "settings"),
         ],
