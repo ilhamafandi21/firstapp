03b62b659cd4cb1bb6a0f91199259c8f1fb35740
ilham <ilhamafandi21@gmail.com>
2025-10-13 23:45:55 +0700

    14-oktober-2025

--- lib/main.dart
+++ lib/main.dart
@@ -35,9 +35,19 @@ class Home extends StatelessWidget {
           ),
         ),
         actions: [
-          Icon(Icons.menu),
+          Icon(Icons.account_circle, color: Colors.brown),
+          Icon(Icons.settings),
         ],
-      
+      ),
+      body: Container(
+        color: Colors.black,
+        margin: EdgeInsets.all(10),
+        padding: EdgeInsets.all(10),
+        child: Icon(
+          Icons.health_and_safety_rounded,
+          size: 100.0,
+          color: const Color.fromARGB(255, 0, 201, 50),
+        ),
       ),
     );
   }
