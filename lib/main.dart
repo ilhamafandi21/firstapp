80271dc30e00534e44d2fee49f003b6a457a093d
ilham <ilhamafandi21@gmail.com>
2025-10-13 22:02:37 +0700

    14-oktober-2025

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,8 @@ class MyApp extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
+  const Home({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
