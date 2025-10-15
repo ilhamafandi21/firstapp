823b9615e4396fdb3d99e257e23ae05760b6daf3
ilham <ilhamafandi21@gmail.com>
2025-10-15 21:48:25 +0700

    15 oktober 2025

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: ' Demo',
+      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
