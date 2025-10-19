0a0c1f1b5a8f7ade921e0b4cd590f807327c0565
ilham <ilhamafandi21@gmail.com>
2025-10-20 01:05:18 +0700

    20 Oktober 2025

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -2,16 +2,13 @@ import 'package:auto_route/auto_route.dart';
 import 'package:firstapp/presentation/dashboard/dashboard.dart';
 import 'package:firstapp/presentation/home/home.dart';
 
-
 part 'app_router.gr.dart';  // wajib untuk generate
 
-// flutter package pub run lean_builder watch --delete-conflicting-outputs
-
 @AutoRouterConfig(replaceInRouteName: 'Page,Route')
-class AppRouter extends _$AppRouter {
+class AppRouter extends RootStackRouter  {
   @override
   List<AutoRoute> get routes => [
-        AutoRoute(page: Home, initial: true),
-        AutoRoute(page: Dashboard),
+        AutoRoute(page: HomePage, initial: true),       // perhatikan HomePage, bukan Home
+        AutoRoute(page: DashboardPage),                // DashboardPage
       ];
 }
