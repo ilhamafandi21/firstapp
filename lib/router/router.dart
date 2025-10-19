e8939158a6332eb731ad9623bf659d997a293dee
ilham <ilhamafandi21@gmail.com>
2025-10-20 01:56:32 +0700

    20 oktober 2025

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,14 +1,11 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:firstapp/presentation/dashboard/dashboard.dart';
-import 'package:firstapp/presentation/home/home.dart';
 
-part 'app_router.gr.dart';  // wajib untuk generate
+@AutoRouterConfig()
+class AppRouter extends RootStackRouter {
 
-@AutoRouterConfig(replaceInRouteName: 'Page,Route')
-class AppRouter extends RootStackRouter  {
   @override
   List<AutoRoute> get routes => [
-        AutoRoute(page: HomePage, initial: true),       // perhatikan HomePage, bukan Home
-        AutoRoute(page: DashboardPage),                // DashboardPage
-      ];
+    /// routes go here
+  ];
 }
+
