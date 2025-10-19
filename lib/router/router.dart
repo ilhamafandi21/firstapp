1b97c578d9e71c2d2aaa9839a39e83267ea0ef13
ilham <ilhamafandi21@gmail.com>
2025-10-20 03:16:27 +0700

    20 Oktober 2025

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,24 +1,15 @@
 import 'package:auto_route/auto_route.dart';
 
-@AutoRouterConfig(replaceInRouteName: 'Screen|Page,Route')
+@AutoRouterConfig()
 class AppRouter extends RootStackRouter {
 
   @override
-  RouteType get defaultRouteType => RouteType.material(); //.cupertino, .adaptive ..etc
-
-  @override
   List<AutoRoute> get routes => [
-    // HomeScreen is generated as HomeRoute because
-    // of the replaceInRouteName property
-    AutoRoute(page: HomeRoute.page),
-  ];
-
-  @override
-  List<AutoRouteGuard> get guards => [
-    // optionally add root guards here
+    AutoRoute(page: HomeScreen, initial: true),
   ];
 }
 
+
 @RoutePage()
-class HomeRoute extends StatefulWidget {}
+class HomeScreen extends StatefulWidget {}
 
--- lib/router/router.gr.dart
+++ lib/router/router.gr.dart
@@ -1,9 +1,19 @@
+// dart format width=80
+// GENERATED CODE - DO NOT MODIFY BY HAND
 
+// **************************************************************************
+// AutoRouterGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:auto_route/auto_route.dart' as _i2;
 import 'package:firstapp/router/router.dart' as _i1;
 
 /// generated route for
-/// [_i1.HomeRoute]
+/// [_i1.HomeScreen]
 class HomeRoute extends _i2.PageRouteInfo<void> {
   const HomeRoute({List<_i2.PageRouteInfo>? children})
     : super(HomeRoute.name, initialChildren: children);
@@ -13,7 +23,7 @@ class HomeRoute extends _i2.PageRouteInfo<void> {
   static _i2.PageInfo page = _i2.PageInfo(
     name,
     builder: (data) {
-      return _i1.HomeRoute();
+      return _i1.HomeScreen();
     },
   );
 }
