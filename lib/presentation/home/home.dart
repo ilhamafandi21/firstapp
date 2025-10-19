54b81440b7c2b2638e8ad5add1995f08cad52018
ilham <ilhamafandi21@gmail.com>
2025-10-19 23:32:00 +0700

    19-oktober 2025

--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -31,7 +31,7 @@ class Home extends StatelessWidget {
               style: ElevatedButton.styleFrom(backgroundColor: Colors.lime),
               onPressed: () {
                 String title = 'Dashboard';
-                String buttonText = 'Back to Home';
+                String buttonText = 'Back to';
                 Navigator.of(context).push(
                   MaterialPageRoute(
                     builder: (context) =>
