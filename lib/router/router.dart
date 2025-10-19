0338e81fab5fbc0d3812789f8fc23b105916379d
ilham <ilhamafandi21@gmail.com>
2025-10-20 02:17:03 +0700

    20 Oktober 2025

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -5,6 +5,22 @@ class AppRouter extends RootStackRouter {
 
   @override
   List<AutoRoute> get routes => [
+        AutoRoute(page: HomeScreen, initial: true),
+        AutoRoute(page: DashboardScreen),
   ];
 }
 
+@RoutePage()
+class HomeScreen extends StatefulWidget {}
+
+@RoutePage()
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Home')),
+    );
+  }
+}
