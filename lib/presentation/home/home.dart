418ef70a1a0595b97b2ca717b4fbc6e28bf94c31
ilham <ilhamafandi21@gmail.com>
2025-10-19 23:32:39 +0700

    19-oktober 2025

--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -39,7 +39,7 @@ class Home extends StatelessWidget {
                   ),
                 );
               },
-              child: Text('Go to Dashboard'),
+              child: Text('Go to '),
             ),
           ),
           Container(
