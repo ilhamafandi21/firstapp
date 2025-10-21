962fcb0605ec60ff1dd4a534414375a60182345a
ilham <ilhamafandi21@gmail.com>
2025-10-21 23:53:01 +0700

    21 Oktober 2025

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,6 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 
-// dart run lean_builder build --delete-conflicting--outputs
+// dart run lean_builder build --delete-conflicting-outputs
 
 @AutoRouterConfig()
 class AppRouter extends RootStackRouter {
