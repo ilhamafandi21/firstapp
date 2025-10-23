a0a5612afa89abc2ba83e05623526e53bbaf27ca
ilham <ilhamafandi21@gmail.com>
2025-10-23 23:33:39 +0700

    23 Oktober 2025

--- lib/presentation/menu/menu_list.dart
+++ lib/presentation/menu/menu_list.dart
@@ -29,7 +29,7 @@ class _MenuListState extends State<MenuList> {
               crossAxisSpacing: 10,
               mainAxisSpacing: 10,
             ),
-            itemBuilder: (context, index) => (
+            itemBuilder: (context, index) => Container(
              
               child: Image.asset(gambarMenu[index], fit: BoxFit.cover),
             ),
