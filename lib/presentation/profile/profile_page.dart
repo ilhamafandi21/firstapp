f23735c60689646e460bad80495e9277a41818af
ilham <ilhamafandi21@gmail.com>
2025-10-24 01:34:23 +0700

    24 Oktober 2025

--- lib/presentation/profile/profile_page.dart
+++ lib/presentation/profile/profile_page.dart
@@ -15,60 +15,75 @@ class _ProfileState extends State<Profile> {
         child: Container(
           child: Container(
             height: 250,
-            color: Colors.red,
-            child: Card(
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Image.asset(
-                        'assets/images/food1.webp',
-                        width: 100,
-                        height: 100,
-                      ),
-          
-                      Column(
-                        children: [
-                          Column(children: [Text('Username'), Text('Job Title')]),
-                          Column(
-                            children: [
-                              Row(
-                                children: [
-                                  Column(
-                                    children: [Text('Articles'), Text('34')],
-                                  ),
-          
-                                  Column(
-                                    children: [Text('followers'), Text('1120')],
-                                  ),
-                                  Column(children: [Text('Rate'), Text('9.3')]),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-          
-                  Row(
-                    children: [
-                      ElevatedButton(onPressed: () {}, child: Text('Chat')),
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: Text('Follow'),
-                        style: ButtonStyle(
-                          backgroundColor: WidgetStatePropertyAll(
-                            Colors.lightBlue,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+            margin: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(254, 255, 253, 1),
+              borderRadius: BorderRadius.circular(12),
+              boxShadow: [
+                BoxShadow(
+                  color: const Color.fromARGB(255, 0, 0, 0),
+                  blurRadius: 5.0,
+                  offset: Offset.fromDirection(90),
+                ),
+              ],
             ),
           ),
+          // child: Container(
+          //   height: 250,
+          //   color: Colors.red,
+          //   child: Card(
+          //     child: Column(
+          //       children: [
+          //         Row(
+          //           children: [
+          //             Image.asset(
+          //               'assets/images/food1.webp',
+          //               width: 100,
+          //               height: 100,
+          //             ),
+
+          //             Column(
+          //               children: [
+          //                 Column(children: [Text('Username'), Text('Job Title')]),
+          //                 Column(
+          //                   children: [
+          //                     Row(
+          //                       children: [
+          //                         Column(
+          //                           children: [Text('Articles'), Text('34')],
+          //                         ),
+
+          //                         Column(
+          //                           children: [Text('followers'), Text('1120')],
+          //                         ),
+          //                         Column(children: [Text('Rate'), Text('9.3')]),
+          //                       ],
+          //                     ),
+          //                   ],
+          //                 ),
+          //               ],
+          //             ),
+          //           ],
+          //         ),
+
+          //         Row(
+          //           children: [
+          //             ElevatedButton(onPressed: () {}, child: Text('Chat')),
+          //             ElevatedButton(
+          //               onPressed: () {},
+          //               child: Text('Follow'),
+          //               style: ButtonStyle(
+          //                 backgroundColor: WidgetStatePropertyAll(
+          //                   Colors.lightBlue,
+          //                 ),
+          //               ),
+          //             ),
+          //           ],
+          //         ),
+          //       ],
+          //     ),
+          //   ),
+          // ),
         ),
       ),
     );
