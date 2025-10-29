a1fc278442ee498c9b6c67a0675d67c9dbe62bd7
ilham <ilhamafandi21@gmail.com>
2025-10-30 00:31:07 +0700

    30-OKTOBER-2025

--- lib/presentation/dashboard/dashboard.dart
+++ lib/presentation/dashboard/dashboard.dart
@@ -1,10 +1,11 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:firstapp/presentation/sign_in/sign_in_page.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key, @required this.title, @required this.buttonText});
-  
+
   final String? title;
   final String? buttonText;
 
@@ -12,12 +13,26 @@ class Dashboard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(title ?? 'Dashboard')),
-      body: ElevatedButton(
-        onPressed: () {
-          // context.router.pop();
-          Navigator.of(context).pop();
-        },
-        child: Text(buttonText ?? 'Button'),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              // context.router.pop();
+              Navigator.of(context).pop();
+            },
+            child: Text(buttonText ?? 'Button'),
+          ),
+
+          ElevatedButton(
+            onPressed: () {
+              // context.router.pop();
+              Navigator.of(
+                context,
+              ).push(MaterialPageRoute(builder: (context) => SignInPage()));
+            },
+            child: Text('Go to sign page'),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/presentation/sign_in/sign_in_page.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({super.key});
+  Widget _buildPageContent() {
+    return Container(
+      padding: const EdgeInsets.all(20.0),
+      color: Colors.grey.shade800,
+      child: ListView(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              const SizedBox(
+                height: 50,
+              ),
+              const SizedBox(
+                width: 200,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              const ListTile(
+                  title: TextField(
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                    hintText: "Email address:",
+                    hintStyle: TextStyle(color: Colors.white70),
+                    border: InputBorder.none,
+                    icon: Icon(
+                      Icons.email,
+                      color: Colors.white30,
+                    )),
+              )),
+              Divider(
+                color: Colors.grey.shade600,
+              ),
+              const ListTile(
+                  title: TextField(
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                    hintText: "Password:",
+                    hintStyle: TextStyle(color: Colors.white70),
+                    border: InputBorder.none,
+                    icon: Icon(
+                      Icons.lock,
+                      color: Colors.white30,
+                    )),
+              )),
+              Divider(
+                color: Colors.grey.shade600,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.cyan,
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(color: Colors.white70, fontSize: 16.0),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Text(
+                'Forgot your password?',
+                style: TextStyle(color: Colors.grey.shade500),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildPageContent(),
+    );
+  }
+}
