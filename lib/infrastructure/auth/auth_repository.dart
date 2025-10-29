00c305dc9c47cab244cc6f4fa6225a02019e3bea
ilham <ilhamafandi21@gmail.com>
2025-10-30 02:01:59 +0700

    30-OKTOBER-2025

--- lib/infrastructure/auth/auth_repository.dart
+++ lib/infrastructure/auth/auth_repository.dart
@@ -1,7 +1,10 @@
-
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 
-class AuthRepository{
-Dio _dio = Dio();
+class AuthRepository {
+  final dio = Dio();
 
+  Future<String> signInUserWithPass({@required username, @required password}) async {
+    Response
+  }
 }
