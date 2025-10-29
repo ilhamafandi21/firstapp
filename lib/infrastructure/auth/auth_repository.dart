87e4289905241ea7d809887fd10c5b367dd8cae8
ilham <ilhamafandi21@gmail.com>
2025-10-30 02:10:56 +0700

    30-OKTOBER-2025

--- lib/infrastructure/auth/auth_repository.dart
+++ lib/infrastructure/auth/auth_repository.dart
@@ -2,9 +2,24 @@ import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 
 class AuthRepository {
-  final dio = Dio();
+  Dio _dio = Dio();
 
-  Future<String> signInUserWithPass({@required username, @required password}) async {
-    Response
+  Future<String> signInUserWithPass({
+    @required username,
+    @required password,
+  }) async {
+    Response _response;
+
+    Map<String, dynamic> requestData = {
+      "username": username,
+      "password": password,
+    };
+
+    _response = await _dio.post(
+      'https://dummyjson.com/user/login',
+      data: requestData,
+    );
+    String _result = _response.data.toString();
+    return _result;
   }
 }
