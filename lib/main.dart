a70ac404d3977ff07cd0e1d4d5971c59bbe4b26f
ilham <ilhamafandi21@gmail.com>
2025-10-29 21:53:04 +0700

    29-OKTOBER-2025

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:firstapp/application/example_provider.dart';
 import 'package:firstapp/presentation/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:nested/nested.dart';
+import 'package:provider/provider.dart';
 // import 'router/router.dart';
 
 void main() {
@@ -12,19 +15,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // /return MaterialApp.router(
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return MultiProvider(
+      providers: <SingleChildWidget>[
+        ChangeNotifierProvider(create: (context) => ExampleProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
 
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 236, 226, 226),
-          // contrastLevel: 1,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 236, 226, 226),
+            // contrastLevel: 1,
+          ),
+          // useMaterial3: true,
+          // useSystemColors: true,
         ),
-        // useMaterial3: true,
-        // useSystemColors: true,
+        // routerConfig: _appRouter.config(),
+        home: Home(),
       ),
-      // routerConfig: _appRouter.config(),
-      home: Home(),
     );
   }
 }
