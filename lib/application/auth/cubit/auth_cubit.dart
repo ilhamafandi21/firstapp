8e715d5c81325c5c2b820b25cd86bbeaf0987bed
ilham <ilhamafandi21@gmail.com>
2025-10-30 06:33:57 +0700

    30 Oktober 2025

--- lib/application/auth/cubit/auth_cubit.dart
+++ lib/application/auth/cubit/auth_cubit.dart
@@ -1,8 +1,15 @@
 import 'package:bloc/bloc.dart';
+import 'package:firstapp/infrastructure/auth/auth_repository.dart';
 import 'package:meta/meta.dart';
 
 part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
   AuthCubit() : super(AuthInitial());
+
+  AuthRepository _authRepository =  AuthRepository();
+
+  void signUser(String username, String password){
+    
+  }
 }
--- lib/application/auth/cubit/auth_state.dart
+++ lib/application/auth/cubit/auth_state.dart
@@ -4,3 +4,12 @@ part of 'auth_cubit.dart';
 sealed class AuthState {}
 
 final class AuthInitial extends AuthState {}
+final class AuthLoading extends AuthState {}
+final class AuthError extends AuthState {
+  final String errorMessage;
+  AuthError(this.errorMessage);
+}
+final class AuthSignSuccess extends AuthState {
+  final String dataLogin;
+  AuthSignSuccess(this.dataLogin);
+}
