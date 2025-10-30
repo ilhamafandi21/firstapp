72df3cff72b718b21ee21ea1e55f85b6438dee48
ilham <ilhamafandi21@gmail.com>
2025-10-30 07:26:16 +0700

    30 Oktober 2025

--- lib/presentation/sign_in/sign_in_page.dart
+++ lib/presentation/sign_in/sign_in_page.dart
@@ -1,5 +1,8 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:firstapp/application/auth/cubit/auth_cubit.dart';
 import 'package:firstapp/infrastructure/auth/auth_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
@@ -15,97 +18,94 @@ class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:  Container(
-      padding: const EdgeInsets.all(20.0),
-      color: Colors.grey.shade800,
-      child: ListView(
-        children: <Widget>[
-          Column(
-            children: <Widget>[
-              const SizedBox(
-                height: 50,
-              ),
-              const SizedBox(
-                width: 200,
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              ListTile(
-                  title: TextField(
-                    controller: _emailController,
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                    hintText: "Email address:",
-                    hintStyle: TextStyle(color: Colors.white70),
-                    border: InputBorder.none,
-                    icon: Icon(
-                      Icons.email,
-                      color: Colors.white30,
-                    )),
-              )),
-              Divider(
-                color: Colors.grey.shade600,
-              ),
-              ListTile(
-                  title: TextField(
-                    controller: _passwordController,
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                    hintText: "Password:",
-                    hintStyle: TextStyle(color: Colors.white70),
-                    border: InputBorder.none,
-                    icon: Icon(
-                      Icons.lock,
-                      color: Colors.white30,
-                    )),
-              )),
-              Divider(
-                color: Colors.grey.shade600,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
+      body: BlocProvider(
+        create: (context) => AuthCubit(),
+        child: BlocConsumer<AuthCubit, AuthState>(
+          listener: (context, state) {
+            if (state is AuthError) {
+              print(state.errorMessage);
+            } else if (state is AuthLoading) {
+              print('loading ...');
+            } else if (state is AuthSignSuccess) {
+              print(state.dataLogin);
+            }
+          },
+          builder: (context, state) {
+            return Container(
+              padding: const EdgeInsets.all(20.0),
+              color: Colors.grey.shade800,
+              child: ListView(
                 children: <Widget>[
-                  Expanded(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.cyan,
+                  Column(
+                    children: <Widget>[
+                      const SizedBox(height: 50),
+                      const SizedBox(width: 200),
+                      const SizedBox(height: 50),
+                      ListTile(
+                        title: TextField(
+                          controller: _emailController,
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            hintText: "Email address:",
+                            hintStyle: TextStyle(color: Colors.white70),
+                            border: InputBorder.none,
+                            icon: Icon(Icons.email, color: Colors.white30),
+                          ),
+                        ),
                       ),
-                      onPressed: () {
-                        // Panggil Cubit
-
-
-
-
-
-
-
-
-
-
-                      },
-                      child: const Text(
-                        'Login',
-                        style: TextStyle(color: Colors.white70, fontSize: 16.0),
+                      Divider(color: Colors.grey.shade600),
+                      ListTile(
+                        title: TextField(
+                          controller: _passwordController,
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            hintText: "Password:",
+                            hintStyle: TextStyle(color: Colors.white70),
+                            border: InputBorder.none,
+                            icon: Icon(Icons.lock, color: Colors.white30),
+                          ),
+                        ),
                       ),
-                    ),
+                      Divider(color: Colors.grey.shade600),
+                      const SizedBox(height: 20),
+                      Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.cyan,
+                              ),
+                              onPressed: () {
+                                // Panggil Cubit
+                                context.read<AuthCubit>().signUser(
+                                  _emailController.text,
+                                  _passwordController.text,
+                                );
+                              },
+                              child: const Text(
+                                'Login',
+                                style: TextStyle(
+                                  color: Colors.white70,
+                                  fontSize: 16.0,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 40),
+                      Text(
+                        'Forgot your password?',
+                        style: TextStyle(color: Colors.grey.shade500),
+                      ),
+                    ],
                   ),
                 ],
               ),
-              const SizedBox(
-                height: 40,
-              ),
-              Text(
-                'Forgot your password?',
-                style: TextStyle(color: Colors.grey.shade500),
-              )
-            ],
-          ),
-        ],
+            );
+          },
+        ),
       ),
-    ),
     );
   }
 }
