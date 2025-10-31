e24b3f6911682b8c749eaa5b6da63de3f1dce925
ilham <ilhamafandi21@gmail.com>
2025-11-01 00:38:06 +0700

    01 Nov 2025

--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -19,7 +19,7 @@ class Home extends StatelessWidget {
         appBar: AppBar(
           // backgroundColor: Color.fromARGB(255, 193, 255, 7),
           title: const Text(
-            "Fasting App",
+            "Fasting ",
             style: TextStyle(
               color: Colors.brown,
               fontSize: 14,
