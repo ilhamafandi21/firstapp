c1437e15b62a537d8e3df984f1330eeb263f5178
ilham <ilhamafandi21@gmail.com>
2025-10-31 22:50:30 +0700

    31 Oktober 2025

--- lib/infrastructure/auth/auth_repository.dart
+++ lib/infrastructure/auth/auth_repository.dart
@@ -1,33 +1,8 @@
-import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
-import 'package:firstapp/domain/auth/model/login_request.dart';
-import 'package:firstapp/domain/auth/model/login_response.dart';
-import 'package:flutter/foundation.dart';
 
-class AuthRepository {
-  final _dio = Dio();
+final dio = Dio();
 
-  Future<Either<String, LoginResponse>> signInUserWithPass({
-    @required LoginRequest? loginRequest,
-  }) async {
-    Response _response;
-
-    try {
-      _response = await _dio.post(
-        'https://dummyjson.com/user/login',
-        data: login_request.toJson(),
-      );
-      LoginResponse _loginResp = LoginResponse.fromJson(_response.data);
-      return right(_loginResp);
-    } on DioException catch (e) {
-
-      switch (e.type) {
-        case DioExceptionType.connectionTimeout:
-         break;
-        case DioExceptionType.badResponse:
-        break;
-
-      }
-    }
-  }
+void getHttp() async {
+  final response = await dio.get('https://dummyjson.com/users');
+  print(response);
 }
